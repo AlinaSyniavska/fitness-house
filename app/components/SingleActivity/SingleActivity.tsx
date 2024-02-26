4b5c9aa1579d48fe1080ea3640fb10b9807d461b
AlinaSyniavska <alina22syniavska@gmail.com>
2024-02-26 19:48:21 +0200

    Implemented randomly performed activities

--- app/components/SingleActivity/SingleActivity.tsx
+++ app/components/SingleActivity/SingleActivity.tsx
@@ -1,48 +1,49 @@
-import React, { FC } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, {FC} from 'react';
+import {StyleSheet, View} from 'react-native';
 
-import { globalColors } from '../../constants/colors';
+import {globalColors} from '../../constants/colors';
 import CustomProgressBar from '../CustomProgressBar/CustomProgressBar';
 import Title from '../Title/Title';
+import {commonHelper} from "../../helpers/common.helper";
 
 interface IProps {
-	title: string;
-	subtitle: string;
-	kcal: number;
-	type: string;
+  title: string;
+  subtitle: string;
+  kcal: number;
+  type: string;
 }
 
-const SingleActivity: FC<IProps> = ({ title, subtitle, kcal }) => {
-	return (
-		<View style={styles.container}>
-			<Title
-				title={title}
-				titleSize={16}
-				subtitle={subtitle}
-				subtitleSize={14}
-			/>
+const SingleActivity: FC<IProps> = ({title, subtitle, kcal, type}) => {
+  return (
+    <View style={styles.container}>
+      <Title
+        title={title}
+        titleSize={16}
+        subtitle={subtitle}
+        subtitleSize={14}
+      />
 
-			<View style={styles.progressBarContainer}>
-				<CustomProgressBar progress={0.3} color={'#000'} />
-				<CustomProgressBar progress={0.7} color={globalColors.pink} />
-			</View>
-		</View>
-	);
+      <View style={styles.progressBarContainer}>
+        <CustomProgressBar progress={commonHelper.getProgress(type, +title)} color={'#000'}/>
+        <CustomProgressBar progress={(Math.random() * (0.9 - 0.4) + 0.4)} color={globalColors.pink}/>
+      </View>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		width: '100%',
-	},
-	progressBarContainer: {
-		height: 50,
-		marginTop: 60,
-		flexDirection: 'column',
-		transform: [{ rotate: '-90deg' }],
-		gap: 10,
-		flexWrap: 'wrap',
-	},
+  container: {
+    flex: 1,
+    width: '100%',
+  },
+  progressBarContainer: {
+    height: 50,
+    marginTop: 60,
+    flexDirection: 'column',
+    transform: [{rotate: '-90deg'}],
+    gap: 10,
+    flexWrap: 'wrap',
+  },
 });
 
 export default SingleActivity;
